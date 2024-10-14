ff0cc662b233cd6c7c3ee2b904a6e94378a0d8b4
waseemrahii <wasimrahii98@gmail.com>
2024-10-14 10:56:30 -0700

    Update categorySlice.js

--- src/components/redux/categorySlice.js
+++ src/components/redux/categorySlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 // API Endpoints
-const API_URL = 'http://localhost:3000/api/categories';
+const API_URL = 'https://pharmacy-server-1.onrender.com/api/categories';
 
 // Fetch categories
 export const fetchCategories = createAsyncThunk(
